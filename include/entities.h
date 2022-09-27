12caa8a303d5e2c049e5cc3cd4c2e860a546d404
jcacostas <juancho--acosta@outlook.es>
2022-09-27 11:54:29 -0500

    files

---
+++ include/entities.h
@@ -0,0 +1,44 @@
+#ifndef ENTITIES
+#define ENTITIES
+
+#include <stdint.h>
+#include <Arduino.h>
+
+class Device {
+    private:
+        uint8_t id;
+        String reference;
+    public:
+        Device(uint8_t,String);
+        uint8_t getId();
+        String getReference();
+};
+Device::Device(uint8_t id, String reference){
+    this->id = id;
+    this->reference = reference;
+};
+uint8_t Device::getId(){
+    return this->id;
+};
+String Device::getReference(){
+    return this->reference;
+};
+
+class Sensor : public Device {
+    private:
+        String variable;
+    protected:
+        double measure;
+    public:
+        Sensor(uint8_t id, String reference, String variable): Device(id, reference){
+            this->variable = variable;
+        };
+        virtual double getMeasure()=0;
+        String getVariable();
+};
+
+String Sensor::getVariable(){
+    return this->variable;
+};
+
+#endif
